1259b0b2c281a45c6f527a393eb32e5da134c907
maryannegathura <mariannegathura7@gmail.com>
2024-07-22 16:47:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// helpers.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraph = document.querySelector('p');
+    if (paragraph) {
+      paragraph.textContent = 'This is really cool!';
+    }
+  });
+  
